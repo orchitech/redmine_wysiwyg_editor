83284104de0cb5c3d9009711e004556186ca9a9e
Michal Bartoš <michal.bartos@orchitech.cz>
2020-10-06 17:57:19 +0200

    WIP Integrate suggested improvements. Add tests.

--- assets/javascripts/RedmineFormatPlugin.js
+++ assets/javascripts/RedmineFormatPlugin.js
@@ -7,41 +7,40 @@
   tinymce.PluginManager.add('redmineformat', function (editor) {
     var $ = editor.$;
 
-    editor.on('PreProcess', function (e) {
-      $('pre', e.node).each(function (index, node) {
-        replaceContentWithInnerText(node);
+    editor.on('SetContent', function () {
+      $('pre').filter(function (index, node) {
+        return !!codeBlockLanguage(node);
+      }).each(function (index, node) {
+        var language = codeBlockLanguage(node);
+        replaceBrWithNl(node);
+        node.innerHTML = editor.dom.encode(node.textContent);
+        node.className = "language-" + language;
       });
     });
 
-    editor.on('SetContent', function () {
-      $('pre').filter(function (index, node) {
-        return !!codeLanguageFromClassName(codeBlockCodeNode(node).className);
-      }).each(function (index, node) {
-        var codeNode = codeBlockCodeNode(node);
-        replaceContentWithInnerText(node);
-        node.innerHTML = editor.dom.encode(node.textContent);
-        node.className = "language-" + codeLanguageFromClassName(codeNode.className);
+    editor.on('PreProcess', function (e) {
+      $('pre', e.node).each(function (index, node) {
+        replaceBrWithNl(node);
+        node.normalize();
       });
     });
   });
 
-  function replaceContentWithInnerText(node) {
+  function replaceBrWithNl(node) {
     $(node).find('br').each(function (index, node) {
       node.parentNode.replaceChild(document.createTextNode('\n'), node);
     });
   }
 
-  function codeBlockCodeNode(preNode) {
+  function codeBlockLanguage(preNode) {
     var singleChild = preNode.childNodes.length === 1 && preNode.firstChild;
-    return singleChild && singleChild.nodeName === 'CODE' && singleChild.className ? singleChild : preNode;
-  }
-
-  function codeLanguageFromClassName(className) {
-    if (!className) {
+    var codeNode = singleChild && singleChild.nodeName === 'CODE'
+      && singleChild.className ? singleChild : preNode;
+    if (!codeNode.className) {
       return null;
     }
     return CODE_CLASS_PATTERNS.reduce(function (match, regexp) {
-      return match || (className.match(regexp) || [null, null])[1];
+      return match || (codeNode.className.match(regexp) || [null, null])[1];
     }, null);
   }
 })();
